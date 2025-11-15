c2a068123477c54993323715d41624860e4e2623
Crawz <realcoder2023@gmail.com>
2025-11-15 11:34:30 -0500

    Added Positions Routes

--- src/Main.kt
+++ src/Main.kt
@@ -5,6 +5,7 @@ import java.awt.EventQueue
 import java.awt.FlowLayout
 import java.awt.GridBagLayout
 import java.net.URI
+import java.util.Locale.getDefault
 import javax.swing.JButton
 import javax.swing.JCheckBox
 import javax.swing.JFrame
@@ -15,6 +16,29 @@ class CreatePage : JFrame() {
     init {
         setSize(300, 500)
         isVisible = true
+    }
+}
+
+class Positions {
+
+    fun getX(x: Int): Int {
+        return x
+    }
+
+    fun getY(y: Int): Int {
+        return y
+    }
+
+    fun getZ(z: Int): Int {
+      return z
+    }
+}
+
+class StringsExtended {
+
+    fun createStringOutput(str: String ): String {
+        return str
+
     }
 }
 
@@ -30,14 +54,18 @@ class  HomeScreen : JFrame() {
         val nameTitle = JLabel("YouTubeChannelOpenerURL")
 
         val f = JTextField(20)
+        val str = StringsExtended()
+            .createStringOutput("XpertThief".uppercase(getDefault()))
+        val str2 = StringsExtended().createStringOutput("iCrazyTeddy".uppercase(getDefault()))
+
+        val pos = Positions()
 
         button.addActionListener {
 
             if (f.getText().contains("Admin")) {
 
-
-                contentPane.background = Color(21 , 71, 52)
-                label.foreground = Color(255, 255, 255)
+                contentPane.background = Color(pos.getX(21), pos.getY(71), pos.getZ(52))
+                label.foreground = Color(pos.getX(255), pos.getY(255), pos.getZ(255))
                 println("Hello World")
                 label.text = "| Welcome Admin |"
                 label.font = font.deriveFont(18)
@@ -49,23 +77,23 @@ class  HomeScreen : JFrame() {
                 win.layout = GridBagLayout()
                 add(nameTitle, BorderLayout.SOUTH)
 
-                val checkBoxOne = JCheckBox("XpertThief")
+                val checkBoxOne = JCheckBox(str)
                 win.add(checkBoxOne)
                 checkBoxOne.addActionListener {
 
-                    val url = URI("www.youtube.com/XpertThief")
+                    val url = URI("www.youtube.com/$str")
                     Desktop.getDesktop().browse(URI.create(url.toString()) as URI)
 
                 }
 
 
-                val checkBoxTwo = JCheckBox("iCrazyTeddy")
+                val checkBoxTwo = JCheckBox(str2)
                 win.add(checkBoxTwo)
 
-                contentPane.background = Color(255, 255, 255)
+                contentPane.background = Color(pos.getX(255), pos.getY(255), pos.getZ(255))
 
                 checkBoxTwo.addActionListener {
-                    val url = URI("www.youtube.com/iCrazyTeddy")
+                    val url = URI("www.youtube.com/$str2")
                     Desktop.getDesktop().browse(URI.create(url.toString()) as URI)
                 }
 
